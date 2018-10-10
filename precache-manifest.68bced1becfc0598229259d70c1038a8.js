33c77549ad5e84d206ddccc962855d5fd3b01e47
Tyler Darnell <tyler.a.darnell@gmail.com>
2018-10-10 21:44:28 +0000

    Updates

---
+++ precache-manifest.68bced1becfc0598229259d70c1038a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d646f7dd9b13d26591b",
+    "url": "/ChicagoBusinessLookups/static/js/runtime~main.7d646f7d.js"
+  },
+  {
+    "revision": "349a81ee8bb2c45f15dc",
+    "url": "/ChicagoBusinessLookups/static/js/main.349a81ee.chunk.js"
+  },
+  {
+    "revision": "59f3c4a6118aff873b11",
+    "url": "/ChicagoBusinessLookups/static/js/1.59f3c4a6.chunk.js"
+  },
+  {
+    "revision": "349a81ee8bb2c45f15dc",
+    "url": "/ChicagoBusinessLookups/static/css/main.02a5494b.chunk.css"
+  },
+  {
+    "revision": "f6ece2097affd04133464d9acbe8b0d4",
+    "url": "/ChicagoBusinessLookups/index.html"
+  }
+];
