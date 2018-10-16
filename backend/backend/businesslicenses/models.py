a5ee23456cb263f259691a3a3e2c70b41a3abfca
Tyler Darnell <tyler.a.darnell@gmail.com>
2018-10-16 22:27:20 +0000

    created fixture and updated models to simplify process

--- backend/backend/businesslicenses/models.py
+++ backend/backend/businesslicenses/models.py
@@ -4,6 +4,8 @@ class BusinessLicense(models.Model):
     dba_name = models.CharField(max_length=500)
     legal_name = models.CharField(max_length=500)
     license_number = models.IntegerField()
+    expiration_date = models.DateField()
+    zip_code = models.CharField(max_length=12)
 
     def __str__(self):
         return self.dba_name + ": " + self.license_number
