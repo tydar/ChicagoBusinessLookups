e25d136f49b1f0213582b212a683ed94cb0939d5
Tyler Darnell <tyler.a.darnell@gmail.com>
2018-10-16 23:11:54 +0000

    set up models

--- backend/API/businesslicenses/models.py
+++ backend/API/businesslicenses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BusinessLicense(models.Model):
+    dba_name = models.CharField(max_length=250)
+    legal_name = models.CharField(max_length=250)
+    license_number = models.IntegerField()
+    expiration_date = models.DateField()
+    zip_code = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.legal_name + " doing business as " + self.dba_name
