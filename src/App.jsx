5ab32a73e3ce26792952aba2ce7b6cf6549799e1
Tyler Darnell <tyler.a.darnell@gmail.com>
2018-10-21 14:55:36 +0000

    install apollo + set up apollo client

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import LookupForm from './components/LookupForm';
 import withSocrataQuery from './components/withSocrataQuery';
 import SimpleResultsTable from './components/SimpleResultsTable';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
 import { MuiThemeProvider, withStyles } from '@material-ui/core/styles';
+
 import { Helmet } from 'react-helmet';
 import theme from './utils/theme';
+
+import { ApolloProvider } from 'react-apollo';
 
 const styles = (theme) => ({
   root: {
@@ -52,39 +56,41 @@ class App extends Component {
 
   render() {
     const { submitted, query } = this.state;
-    const { classes } = this.props;
+    const { classes, client } = this.props;
     let TableComponent = submitted ? withSocrataQuery(SimpleResultsTable, query) : null;
     return (
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <Helmet>
-          <title>Chicago Business Lookup</title>
-        </Helmet>
-        <div className={classes.root}>
-          <div className={classes.grow}>
-            <AppBar position="static">
-              <Toolbar>
-                <Typography variant="title" color="inherit" className={classes.grow}>
-                  Chicago Business Search
-                </Typography>
-              </Toolbar>
-            </AppBar>
-          </div>
-          <Paper className={classes.root}>
-            <div className={classes.container}>
-              <Typography variant="subheading" color="inherit" className={classes.text}>
-                Quick, dead-simple business lookups using City of Chicago data
-              </Typography>
-              <Typography variant="body2" color="inherit" className={classes.text}>
-                Use this tool to look up the status of business licenses based on their legal or DBA name or zip code. Coming soon: more advanced filtering
-                and the ability to lookup by multiple criteria at once.
-              </Typography>
-              <LookupForm handleSubmit={this.handleSubmit} />
+      <ApolloProvider client={client}>
+        <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <Helmet>
+            <title>Chicago Business Lookup</title>
+          </Helmet>
+          <div className={classes.root}>
+            <div className={classes.grow}>
+              <AppBar position="static">
+                <Toolbar>
+                  <Typography variant="title" color="inherit" className={classes.grow}>
+                    Chicago Business Search
+                  </Typography>
+                </Toolbar>
+              </AppBar>
             </div>
-            {submitted ? <TableComponent /> : null}
-          </Paper>
-        </div>
-      </MuiThemeProvider>
+            <Paper className={classes.root}>
+              <div className={classes.container}>
+                <Typography variant="subheading" color="inherit" className={classes.text}>
+                  Quick, dead-simple business lookups using City of Chicago data
+                </Typography>
+                <Typography variant="body2" color="inherit" className={classes.text}>
+                  Use this tool to look up the status of business licenses based on their legal or DBA name or zip code. Coming soon: more advanced filtering
+                  and the ability to lookup by multiple criteria at once.
+                </Typography>
+                <LookupForm handleSubmit={this.handleSubmit} />
+              </div>
+              {submitted ? <TableComponent /> : null}
+            </Paper>
+          </div>
+        </MuiThemeProvider>
+      </ApolloProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+});
+
+ReactDOM.render(<App client={client} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
